8e7c1401a3c3850370343c171d5a614c3375b746
Sergei Smirnov <sergous@gmail.com>
2022-02-06 05:36:36 -0500

    Add choices voting to question

--- app/pages/questions/[questionId].tsx
+++ app/pages/questions/[questionId].tsx
@@ -3,12 +3,23 @@ import { Head, Link, useRouter, useQuery, useParam, BlitzPage, useMutation, Rout
 import Layout from "app/core/layouts/Layout"
 import getQuestion from "app/questions/queries/getQuestion"
 import deleteQuestion from "app/questions/mutations/deleteQuestion"
+import updateChoice from "app/choices/mutations/updateChoice"
 
 export const Question = () => {
   const router = useRouter()
   const questionId = useParam("questionId", "number")
   const [deleteQuestionMutation] = useMutation(deleteQuestion)
-  const [question] = useQuery(getQuestion, { id: questionId })
+  const [question, { refetch }] = useQuery(getQuestion, { id: questionId })
+  const [updateChoiceMutation] = useMutation(updateChoice)
+
+  const handleVote = async (id: number) => {
+    try {
+      await updateChoiceMutation({ id })
+      refetch()
+    } catch (error) {
+      alert("Error updating choice ")
+    }
+  }
 
   return (
     <>
@@ -18,7 +29,15 @@ export const Question = () => {
 
       <div>
         <h1>Question {question.id}</h1>
-        <pre>{JSON.stringify(question, null, 2)}</pre>
+        <p>{question.text}</p>
+        <ul>
+          {question.choices.map((choice) => (
+            <li key={choice.id}>
+              {choice.text} - {choice.votes} votes
+              <button onClick={() => handleVote(choice.id)}>Vote</button>
+            </li>
+          ))}
+        </ul>
 
         <Link href={Routes.EditQuestionPage({ questionId: question.id })}>
           <a>Edit</a>
